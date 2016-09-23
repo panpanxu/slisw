9f19bfc9943932627922841b5cc4e338e10d66a9
徐艺 <xuyi@mindai.com>
2016-09-23 16:24:53 +0800

    grep

--- ruby/filereader.rb
+++ ruby/filereader.rb
@@ -2,6 +2,8 @@ line_count = 0
 File.open("hello.rb") do |file|
   file.each_line do |line|
     line_count += 1
-    puts "#{line_count}: #{line}"
+    if /line/.match(line)
+      puts "#{line_count}: #{line}"
+    end
   end
 end
