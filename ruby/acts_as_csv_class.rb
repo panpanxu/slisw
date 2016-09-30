d0ff48f381da68791a3f47d8a6b03b9a3ac53446
xuyi <xuyi@tenchang.com>
2016-09-30 11:21:22 +0800

    act as csv

--- ruby/acts_as_csv_class.rb
+++ ruby/acts_as_csv_class.rb
@@ -1,4 +1,4 @@
-class ActsAsCsvV1
+class ActsAsCsv
   def read
     file = File.new(self.class.to_s.downcase + '.txt')
     @headers = file.gets.chomp.split(', ')
@@ -15,28 +15,6 @@ class ActsAsCsvV1
   def initialize
     @result = []
     read
-  end
-end
-
-class ActsAsCsv
-  def self.acts_as_csv
-    define_method "read" do
-      file = File.new(self.class.to_s.downcase + '.txt')
-      @headers = file.gets.chomp.split(', ')
-      file.each do |row|
-        @result << row.chomp.split(', ')
-      end
-    end
-    define_method "headers" do
-      @headers
-    end
-    define_method 'csv_contents' do
-      @result
-    end
-    define_method 'initialize' do
-      @result = []
-      read
-    end
   end
 end
 
